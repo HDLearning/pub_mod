269bbe0445d852fb6661b533582dd49f0c783fc8
Hamza <mlearning51@gmail.com>
2024-08-19 20:54:10 +0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# __manifest__.py
+
+{
+    'name': 'Test Module',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'summary': 'A module to demonstrate a simple test in Odoo',
+    'description': """
+        This is a test module to demonstrate how to create a simple test in Odoo.
+    """,
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+}
